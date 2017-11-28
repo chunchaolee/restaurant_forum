e279f69790ee31efee6e4e4c489f7e06db14d6f7
chunchaolee <chunchaolee@gmail.com>
2017-11-28 16:47:30 +0800

    add carrierwave,ffaker,kaminari gems and install bootstrap

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,11 @@ class RestaurantsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @restaurants = Restaurant.page(params[:page]).per(10)
+  end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
