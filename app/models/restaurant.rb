32d5c21f27b1ab96edbe84b97103d4ddaba92615
chunchaolee <chunchaolee@gmail.com>
2017-12-13 13:50:23 +0800

    create comment model and setup relation between restaurant, user and comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
 
   belongs_to :category
+  has_many :comments
   # 建立表單驗證
   validates_presence_of :name, :tel, :address, :opening_hours, :description
   # uploader photo
