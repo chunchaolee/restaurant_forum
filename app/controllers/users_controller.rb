2ba193e92d3d0ee35fdf65f22244d89322a05a7c
chunchaolee <chunchaolee@gmail.com>
2017-12-15 22:49:30 +0800

    add user controller edit action, update action, edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes!(user_params)
+      redirect_to user_url, :notice => "會員資料更新成功"
+    else
+      render :action => :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar, :name, :intro)
+  end
+
+
 end
