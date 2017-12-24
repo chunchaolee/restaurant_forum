ca6815cd2639d1880ca2c6d43a01323e56b7009b
chunchaolee <chunchaolee@gmail.com>
2017-12-24 15:18:12 +0800

    added favorites_count column into restaurants table

---
+++ db/migrate/20171224071430_add_favorites_count_to_restaurants_table.rb
@@ -0,0 +1,5 @@
+class AddFavoritesCountToRestaurantsTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :restaurants, :favorites_count, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171224034629) do
+ActiveRecord::Schema.define(version: 20171224071430) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171224034629) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "category_id"
+    t.integer "favorites_count"
   end
 
   create_table "users", force: :cascade do |t|
