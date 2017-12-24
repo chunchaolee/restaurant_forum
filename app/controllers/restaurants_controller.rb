02df9b9f4c55167254198602bddfcb71152efa70
chunchaolee <chunchaolee@gmail.com>
2017-12-24 12:26:43 +0800

    added like&unlike method on RestaurantsController, and added like&unlike btn in restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
 
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
   # 認證 > ApplicationController
 
   def index
@@ -8,7 +9,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
+    # set_restaurant before action
     @comment = Comment.new
   end
 
@@ -18,20 +19,41 @@ class RestaurantsController < ApplicationController
   end
 
   def dashboard
-    @restaurant = Restaurant.find(params[:id])
+    # set_restaurant before action
   end
 
   def favorite
-    @restaurant = Restaurant.find(params[:id])
+    # set_restaurant before action
     @restaurant.favorites.create!(user: current_user)
     redirect_back(fallback_location: root_path)
   end
 
   def unfavorite
-    @restaurant = Restaurant.find(params[:id])
+    # set_restaurant before action
     favorites = Favorite.where(restaurant: @restaurant, user: current_user)
     favorites.destroy_all
     redirect_back(fallback_location: root_path)
+  end
+
+  def like
+    # set_restaurant before action
+    @restaurant.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unlike
+    # set_restaurant before action
+    likes = Like.where(restaurant: @restaurant, user: current_user)
+    likes.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
+
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
