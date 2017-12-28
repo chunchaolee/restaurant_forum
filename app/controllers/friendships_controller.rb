d668e5e914471a89272b4c3f9116c880f3993f7b
chunchaolee <chunchaolee@gmail.com>
2017-12-29 00:20:05 +0800

    added validates uniqueness, added destroy method on friendships controller, added destroy button in user index page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,9 +7,16 @@ class FriendshipsController < ApplicationController
       flash[:notice] = "成功成為朋友！"
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = "已為好友"
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id])
+    @friendship.destroy_all
+    flash[:notice] = "成功解除好友！"
+    redirect_back(fallback_location: root_path)
   end
 
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,7 @@
 class Friendship < ApplicationRecord
 
+  validates :friend_id, uniqueness: {scope: :user_id}
+
   # 一筆交友紀錄屬於一個發送交友的user
   belongs_to :user
   # 一筆交友紀錄也屬於一個接受交友的user
